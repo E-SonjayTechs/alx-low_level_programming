4a4901524bd42effb83ebb0fd1413080506fbb72
E-SonjayTechs <sonjayben@gmail.com>
2022-10-14 22:42:11 -0700

    task 6 done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Description: This prints all Hex numbers
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	char hexVals[] = "0123456789abcdef";
+
+	for (n = 0; n <= 16; n++)
+	{
+		putchar(hexVals[n]);
+	}
+	putchar('\n');
+	return (0);
+}
