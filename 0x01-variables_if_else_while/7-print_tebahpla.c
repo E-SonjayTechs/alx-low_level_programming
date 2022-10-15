a15c92e04e805d04bca33ac549246a334b57fee9
E-SonjayTechs <sonjayben@gmail.com>
2022-10-14 22:13:23 -0700

    task 7 done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: This prints alphabets in reverse followed by newline
+ * Return: always 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+	return (0);
+}
