ad04249e5c51ea2e6604e2015928672a323f04ac
E-SonjayTechs <sonjayben@gmail.com>
2022-10-24 11:39:25 -0700

    task 1 done - value swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int -  swaps the values of two integers.
+ * @a: var with integer value one
+ * @b: var with integer value two
+ * Return: always 0
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
