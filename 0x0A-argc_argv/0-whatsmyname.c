7977692198195b354342b40f23ec5605d3bb2f3c
E-SonjayTechs <sonjayben@gmail.com>
2022-11-04 18:13:14 -0700

    task 0 done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point to the program
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
