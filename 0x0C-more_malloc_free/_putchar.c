ab6c95aad0e515d507d532e542c0e51602ae5231
E-SonjayTechs <sonjayben@gmail.com>
2022-11-13 10:35:45 -0800

    Mandatory task 0 and 1 done

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints a character to the standard output
+ * @c: character to be printed
+ * Return: 1 on success, 0 otherwise
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
