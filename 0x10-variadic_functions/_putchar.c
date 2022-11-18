72f1c681ff3ba07f8dd1bc6bf6fd9d35b3ab1e02
E-SonjayTechs <sonjayben@gmail.com>
2022-11-17 16:00:55 -0800

    task 0 to 3 done

---
+++ 0x10-variadic_functions/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints a character tothe standard output
+ * @c: character to print
+ * Return: 1 on success, otherwise 0
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
